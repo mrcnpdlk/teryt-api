60792de968b9b97145ba7ff82356100ffbf9536e
Marcin Pudełek <mpudelek@widzew.net>
2017-09-07 09:58:13 +0200

    phpUnit

---
+++ tests/Test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * TERYT-API
+ *
+ * Copyright (c) 2017 pudelek.org.pl
+ *
+ * For the full copyright and license information, please view source file
+ * that is bundled with this package in the file LICENSE
+ *
+ * Author Marcin Pudełek <marcin@pudelek.org.pl>
+ *
+ */
+
+/**
+ * Created by Marcin Pudełek <marcin@pudelek.org.pl>
+ * Date: 07.09.2017
+ * Time: 08:54
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testFirst()
+    {
+
+    }
+
+}
