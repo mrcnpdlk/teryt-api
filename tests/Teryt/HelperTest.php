a9e5b6baaefa0eda36443684c6de96377d8dd641
Marcin Pudełek <mrcnpdlk@gmail.com>
2017-09-09 20:40:48 +0200

    remove static create() from ResponseModel + Tests

---
+++ tests/Teryt/HelperTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 09.09.2017
+ * Time: 19:59
+ */
+
+namespace Teryt;
+
+use PHPUnit\Framework\TestCase;
+
+class HelperTest extends TestCase
+{
+
+}
---
+++ tests/Teryt/TercTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 09.09.2017
+ * Time: 20:12
+ */
+
+namespace Teryt;
+
+use PHPUnit\Framework\TestCase;
+
+class TercTest extends TestCase
+{
+
+}
